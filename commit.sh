61b31cacf9ca7213b31b6d9f923ae259e982130a
jalarab <jalarab@gmail.com>
2021-03-19 17:12:16 -0500

    made changes

--- commit.sh
+++ commit.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd D:\Tautulli\newsletters
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Regular auto-commit $(timestamp)"
-git push origin master
+git add .
+git commit -am "made changes"
+echo Press Enter...
+read
